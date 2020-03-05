8bfc24f75403c58555fe2e48d1068c3f728b2ab3
TarasBabich <taras.babich@gmail.com>
2020-03-05 17:40:36 +0000

    image-sectiob

--- js/script2.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function(event) {
-        if($(this).scrollTop() > 90) {
-        $(".scroll-mnu").slideDown();
-        return false;
-  	 		}
-    		else {
-        $(".scroll-mnu").slideUp();
-   		 }
-		}); 
